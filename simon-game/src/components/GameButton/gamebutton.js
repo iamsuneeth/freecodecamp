6a577c0d3f277477d891db0cdef4f9deaf97bbac
iamsuneeth <suneeth.lenin@gmail.com>
2017-10-22 19:08:59 +0530

    changed to div from button

--- simon-game/src/components/GameButton/gamebutton.js
+++ simon-game/src/components/GameButton/gamebutton.js
@@ -3,5 +3,5 @@ import propTypes from 'prop-types';
 import './game-button.css';
 
 export default props => (
-    <button disabled={!props.clickable} className={"btn btn-"+props.id+(props.clickable?" clickable":"")+(props.glow===props.id?" glow":"")} id={"button-"+props.id} onClick={() => props.onClick(props.id)}></button>
+    <div disabled={!props.clickable} className={"btn btn-"+props.id+(props.clickable?" clickable":"")+(props.glow===props.id?" glow":"")} id={"button-"+props.id} onClick={() => props.onClick(props.id)}></div>
 )
